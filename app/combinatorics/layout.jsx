4567f63809aee9bbfd5ebb47272a2baf12143ac4
unknown <randalchukzwilson@gmail.com>
2024-12-05 06:38:47 +0100

    Changed BG commit

--- app/combinatorics/layout.jsx
+++ app/combinatorics/layout.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const PageLayout = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-slate-300">
       <header className="mx-auto flex flex-col justify-between w-full py-2 px-5 mb-4 bg-slate-500 sticky top-0 left-0 z-10 shadow-sm">
         <nav className="w-full flex items-center justify-center border-black">
           <Link className="grid place-items-center m-3 w-[38px] px-2 py-1 rounded-lg border border-gray-500 focus:border-2 focus:border-gray-800 bg-gray-300" href="/combinatorics">
