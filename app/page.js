a21e2f273121e8c0ce375f0f29343dbe3cca1843
Rd <97230275+rdghostmann@users.noreply.github.com>
2025-12-29 05:53:42 +0100

    Hide Homepage

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center w-full min-h-screen">
+    <main className="hidden items-center justify-center w-full min-h-screen">
       <section className="w-fit">
         <h1 className="text-center text-3xl ">Welcome to Inverted Right Pyramid</h1>
         
